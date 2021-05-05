70bacafed9dc7682aa5db43600147d6607381261
alainpiloto <78241065+alainpiloto@users.noreply.github.com>
2021-05-04 23:44:14 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$(window).scroll(function(){
-		if( $(this).scrollTop() > 700 ){
+		if( $(this).scrollTop() > 600 ){
 			$('nav').addClass('cabeceraNavsBackground');
 		} else {
 			$('nav').removeClass('cabeceraNavsBackground');
