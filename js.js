aae1ee5ac7dad3322661344a9e8d79ebd1a57820
alainpiloto <78241065+alainpiloto@users.noreply.github.com>
2021-05-20 22:59:43 -0500

    js update

--- js.js
+++ js.js
@@ -1,22 +1,11 @@
 
 
 
+
 $(document).ready(function(){
 
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 100 ){
-			$('#whatsAppFlotante').addClass('showWhatsappIcon') && $('#whatsAppFlotante').removeClass('hideWhatsappIcon');
-		} else {
-			$('#whatsAppFlotante').removeClass('showWhatsappIcon') && $('#whatsAppFlotante').addClass('hideWhatsappIcon');
-		}
-	});
-
-});
-
-$(document).ready(function(){
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 400 ){
 			$('#navbar').addClass('cabeceraNavsBackground');
 		} else {
 			$('#navbar').removeClass('cabeceraNavsBackground');
@@ -24,6 +13,17 @@ $(document).ready(function(){
 	});
 
 });
+
+
+
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 150 ){
+			$('#whatsAppFlotante').addClass('showWhatsappIcon') && $('#whatsAppFlotante').removeClass('hideWhatsappIcon');
+		} else {
+			$('#whatsAppFlotante').removeClass('showWhatsappIcon') && $('#whatsAppFlotante').addClass('hideWhatsappIcon');
+		}
+	});
 
 
 
